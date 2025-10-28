1f38750c63dca15eec56660d6b9b477d7b2e15a0
kitaev_v <kitaev@reezon.ly>
2025-10-28 21:55:54 +0300

    chore(AgGridVue) - добавлен AgGridVue 3

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
+import { AllCommunityModule, ModuleRegistry } from 'ag-grid-community'
+import { RowGroupingModule } from 'ag-grid-enterprise'
 import App from './App.vue'
+
+ModuleRegistry.registerModules([AllCommunityModule, RowGroupingModule])
 
 createApp(App).mount('#app')
